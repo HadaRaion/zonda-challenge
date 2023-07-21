8b2451d949a60d32f0751e971aadc9ff304676f5
hada <sweet.raion@gmail.com>
2023-07-21 03:00:47 -0700

    Complete the employee list page.

--- archive-employee.php
+++ archive-employee.php
@@ -15,13 +15,10 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
+				<h1 class="font-serif font-normal text-center text-7xl">Meet our employees</h1>
 			</header><!-- .page-header -->
 
-			<ul>
+			<ul class="employee-list">
 				<?php
 
 				while ( have_posts() ) :
@@ -31,15 +28,21 @@ get_header();
 					$profile_image = get_field('profile_image');
 					$position_title = get_field('position_title');
 					?>
-					<li>
-						<a class="post" href="<?php the_permalink(); ?>" aria-label="<?php the_title(); ?>">
-							
-							<span class="post__thumbnail">				
-	
-							</span>
-						<h2><?php echo $employee_name; ?></h2>
-            <p><?php echo $position_title; ?></p>
 
+					<li>
+						<a class="employee-card" href="<?php the_permalink(); ?>" aria-label="<?php the_title(); ?>">
+							
+							<span class="employee-card__image">				
+								<img
+									srcset="<?php echo $profile_image['sizes']['employeeProfile']; ?>, <?php echo $profile_image['sizes']['employeeProfile-2x']; ?> 2x"
+									src="<?php echo $profile_image['sizes']['employeeProfile']; ?>"
+									alt="<?php echo $profile_image['alt'] ? $profile_image['alt'] : $employee_name; ?>"
+								/>
+							</span>
+							<span class="employee-card__text">
+								<h2 class="font-serif font-normal text-4xl"><?php echo $employee_name; ?></h2>
+								<h3 class="font-light text-xl text-gray"><?php echo $position_title; ?></h3>
+							<span>
 						</a>
 					</li>
 					
